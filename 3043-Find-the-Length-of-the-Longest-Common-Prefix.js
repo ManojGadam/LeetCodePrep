478c94b9c16e636c845ca5de1e087ff2a9819d1d
Manoj Gadamchetty <64482839+ManojGadam@users.noreply.github.com>
2024-09-24 12:13:59 -0400

    [3043] [Time Beats: 42.86%] [Memory Beats: 5.36%] - LeetPush

---
+++ 3043-Find-the-Length-of-the-Longest-Common-Prefix.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number}
+ */
+class TrieNode{
+    constructor(){
+        this.children = {}
+        this.endOfWord = false
+    }
+}
+var longestCommonPrefix = function(arr1, arr2) {
+    arr1 = arr1.map(x=>x.toString())
+    arr2 = arr2.map(x=>x.toString())
+    let root = new TrieNode(),ret = 0
+    function addToTrie(word){
+        let curr = root
+        for(let i=0;i<word.length;i++){
+            if(!(word[i] in curr.children))curr.children[word[i]] = new TrieNode()
+            curr = curr.children[word[i]]
+        }
+        curr.endOfWord = true
+    }
+    function search(word){
+        let curr = root , len = 0
+        for(let i=0;i<word.length;i++){
+            if(!(word[i] in curr.children))break
+            curr = curr.children[word[i]]
+            len++
+        }
+        return len
+    }
+    for(let i of arr1){
+        addToTrie(i)
+    }
+    //console.log(root)
+    for(let i of arr2){
+       ret = Math.max(ret,search(i))
+    }
+    return ret
+};
