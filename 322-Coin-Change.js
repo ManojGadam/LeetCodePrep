a5c07af252956730077496afb633f60356083aa3
Manoj Gadamchetty <64482839+ManojGadam@users.noreply.github.com>
2024-09-25 11:35:50 -0400

    [322] [Time Beats: 5.04%] [Memory Beats: 5.04%] - LeetPush

--- 322-Coin-Change.js
+++ 322-Coin-Change.js
@@ -4,19 +4,15 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
-     const memo = new Map();
-    
-    function permute(left) {
-        if(memo.has(left)) return memo.get(left)
-        if(left === 0) return 0;
-        let min = Infinity;
-        
-        for(let coin of coins) {
-            if(left-coin >= 0) min = Math.min(min, permute(left-coin));
-        }
-        memo.set(left, min+1);
-        return min + 1;
+    const memo={}
+    const rec=(i,rem)=>{
+        if(i>coins.length-1 || rem<0)return Infinity
+        if(rem == 0)return 0
+        const key = `${i}+${rem}`;
+        if(key in memo)return memo[key]
+        memo[key] = Math.min(1+rec(i,rem-coins[i]), rec(i+1,rem))
+        return memo[key]
     }
-    const result = permute(amount);
-    return result === Infinity ? -1 : result;
+    const ret = rec(0,amount)
+    return ret == Infinity ? -1 : ret
 };
