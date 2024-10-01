8a929d5ac19d1e164188b8bbf191513bc5faa4dd
Manoj Gadamchetty <64482839+ManojGadam@users.noreply.github.com>
2024-10-01 12:31:14 -0400

    [744] [Time Beats: 61.26%] [Memory Beats: 60.98%] - LeetPush

--- 744-Find-Smallest-Letter-Greater-Than-Target.js
+++ 744-Find-Smallest-Letter-Greater-Than-Target.js
@@ -4,8 +4,15 @@
  * @return {character}
  */
 var nextGreatestLetter = function(letters, target) {
-   for(let i of letters){
-    if(i.charCodeAt(0)>target.charCodeAt(0))return i
-   }
-   return letters[0]
+    let s=0,e = letters.length-1,mid = 0
+    while(s<=e){
+        mid = Math.floor((s+e)/2)
+        if(letters[mid].charCodeAt(0)>target.charCodeAt(0) && mid>0 && letters[mid-1].charCodeAt(0)<=target.charCodeAt(0)){
+            return letters[mid]
+        }
+        else if(letters[mid].charCodeAt(0) <= target.charCodeAt(0)) s= mid+1
+        else e = mid-1
+    }
+    if(letters[mid].charCodeAt(0)<=target.charCodeAt(0))return letters[0]
+    return letters[mid]
 };
