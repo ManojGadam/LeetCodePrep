fcb16236f1e43b1cbb0c7c9eaa1a81132129a7fa
Manoj Gadamchetty <64482839+ManojGadam@users.noreply.github.com>
2024-10-24 00:11:23 -0400

    [594] [Time Beats: 98.43%] [Memory Beats: 88.34%] - LeetPush

--- 594-Longest-Harmonious-Subsequence.js
+++ 594-Longest-Harmonious-Subsequence.js
@@ -3,18 +3,14 @@
  * @return {number}
  */
 var findLHS = function(nums) {
-    if (nums.length < 2) return 0;
     
     nums.sort((a, b) => a - b); // Sort the array
     let s = 0, max = 0;
     
     for (let i = 1; i < nums.length; i++) {
-        // Move start pointer if the difference is greater than 1
-        while (nums[i] - nums[s] > 1) {
+        if (nums[i] - nums[s] > 1) {
             s++;
-        }
-        
-        // If we have found a valid subsequence, calculate its length
+        }        
         if (nums[i] - nums[s] === 1) {
             max = Math.max(max, i - s + 1);
         }
