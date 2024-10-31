95074973045bae95926458dd80170391e896168f
Manoj Gadamchetty <64482839+ManojGadam@users.noreply.github.com>
2024-10-31 12:31:11 -0400

    [108] [Time Beats: 35.26%] [Memory Beats: 72.17%] - LeetPush

---
+++ 108-Convert-Sorted-Array-to-Binary-Search-Tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(arr) {
+    let buildBST = (l,r) =>{
+        let mid = Math.floor((l+r)/2)
+        let root = null
+        if(mid>l-1 && mid<r+1 && arr[mid]!=null){
+        root = new TreeNode(arr[mid])
+        root.left = buildBST(l,mid-1)
+        root.right = buildBST(mid+1,r)
+        }
+        return root
+    }
+    return buildBST(0,arr.length-1)
+};
