9d24d3ccadfc1a26373713059e30fdf2b9deddaf
Manoj Gadamchetty <64482839+ManojGadam@users.noreply.github.com>
2024-11-04 11:59:18 -0500

    [3163] [Time Beats: 25.00%] [Memory Beats: 14.29%] - LeetPush

--- 3163-String-Compression-III.js
+++ 3163-String-Compression-III.js
@@ -3,20 +3,16 @@
  * @return {string}
  */
 var compressedString = function(word) {
-    let ret = [] ,count = 1
-    for(let i=0;i<word.length;i++){
-        while(i<word.length && word[i]==word[i+1]){
-            i++
-            count++
-            if(count > 8){
-                ret.push(count+word[i])
-                count = 1
-                i++
-            }
-        }
-        if(i>word.length-1)break
-        ret.push(count+word[i])
-        count = 1
+   let ret = [], n =word.length
+   for(let i=0;i<word.length;){
+    let char = word[i]
+    let  k = i+9
+    let count = 0
+    while(i<k && i<n && word[i] == char){
+        i++
+        count++
     }
-    return ret.join("")
+    ret.push(count+char)
+   }
+    return ret.join("");
 };
