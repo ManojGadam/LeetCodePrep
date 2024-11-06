93a3c15291a1029ed5a4bdfed4a859532d15233d
Manoj Gadamchetty <64482839+ManojGadam@users.noreply.github.com>
2024-11-06 12:39:27 -0500

    [45] [Time Beats: 100.00%] [Memory Beats: 89.34%] - LeetPush

--- 45-Jump-Game-II.js
+++ 45-Jump-Game-II.js
@@ -3,9 +3,8 @@
  * @return {number}
  */
 var jump = function(nums) {
-    if(nums.length<2)return 0
      let jumps = 0, currentEnd = 0, farthest = 0
-     for(let i=0;i<nums.length;i++){
+     for(let i=0;i<nums.length-1;i++){
         farthest = Math.max(farthest,i+nums[i])
         if(i == currentEnd){
             jumps++
