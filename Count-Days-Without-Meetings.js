42e1b314474de85d74200d895d57d7a506ddfb5c
Manoj Gadamchetty <64482839+ManojGadam@users.noreply.github.com>
2025-03-24 18:18:28 -0400

    [3169] [Time Beats: 80.65%] [Memory Beats: 51.61%] - LeetPush

--- Count-Days-Without-Meetings.js
+++ Count-Days-Without-Meetings.js
@@ -5,16 +5,15 @@
  */
 var countDays = function(days, meetings) {
     meetings.sort((a,b)=>a[0]-b[0])
-    let start = meetings[0],dayScheduled = 0,nl = []
+    let start = meetings[0],dayScheduled = 0
     for(let i=1;i<meetings.length;i++){
         if(meetings[i][0]<=start[1]){
-            meetings[i] = [start[0],Math.max(start[1],meetings[i][1])]
+            start = [start[0],Math.max(start[1],meetings[i][1])]
         }
         else{
-            //nl.push(start)
             dayScheduled += start[1]-start[0]+1
+            start = meetings[i]
         }
-        start = meetings[i]
     }
     if(start)dayScheduled += start[1]-start[0]+1
    
