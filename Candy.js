a83f5f44c5db9a1293945fac1f5dd80c21f80091
Manoj Gadamchetty <manojgadam1@gmail.com>
2025-07-19 02:50:25 -0400

    [135] [Time Beats: 26.13%] [Memory Beats: 10.84%] - LeetPush

--- Candy.js
+++ Candy.js
@@ -3,14 +3,20 @@
  * @return {number}
  */
 var candy = function(ratings) {
-    const ret = new Array(ratings.length).fill(1)
-    for(let i=1;i<ratings.length;i++){
-        if(ratings[i]>ratings[i-1])ret[i] = ret[i-1]+1
+    const left = new Array(ratings.length).fill(1),right = new Array(ratings.length).fill(1), n =ratings.length
+    for(let i=1;i<n;i++){
+        if(ratings[i]>ratings[i-1]){
+            left[i] = left[i-1]+1
+        }
     }
-    for(let i=ratings.length-2;i>=0;i--){
-        if(ratings[i]>ratings[i+1] && ret[i]<=ret[i+1])ret[i] = ret[i+1]+1 
+    for(let i=n-2;i>=0;i--){
+        if(ratings[i]>ratings[i+1]){
+            right[i] = right[i+1]+1
+        }
     }
-    let sum = 0
-    for(let i of ret)sum += i
-    return sum
+    let res = 0
+    for(let i=0;i<n;i++){
+        res += Math.max(left[i],right[i])
+    }
+    return res
 };
