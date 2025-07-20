c9e582d76e2c20d2b33fe3fc9ad74bf38ba133ee
Manoj Gadamchetty <manojgadam1@gmail.com>
2025-07-20 17:57:09 -0400

    [73] [Time Beats: 59.99%] [Memory Beats: 66.82%] - LeetPush

--- Set-Matrix-Zeroes.js
+++ Set-Matrix-Zeroes.js
@@ -2,20 +2,36 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var setZeroes = function(matrix) {
-    const m = matrix.length,n = matrix[0].length
-    const r = new Set(),c= new Set()
+var setZeroes = function(arr) {
+    const m = arr.length, n = arr[0].length
+    let rZ = false, cZ = false
     for(let i=0;i<m;i++){
         for(let j=0;j<n;j++){
-            if(matrix[i][j] == 0){
-               r.add(i)
-               c.add(j)
+            if(arr[i][j] == 0){
+                if(i==0)rZ = true
+                if(j==0)cZ = true
+                    arr[0][j] = 0
+                    arr[i][0] = 0
+                
             }
         }
     }
-     for(let i=0;i<m;i++){
-        for(let j=0;j<n;j++){
-            if(r.has(i) || c.has(j))matrix[i][j] = 0
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (arr[i][0] === 0 || arr[0][j] === 0) {
+                arr[i][j] = 0;
+            }
+        }
+    }
+
+    if(rZ){
+        for(let i=1;i<n;i++){
+            arr[0][i] = 0
+        }
+    }
+    if(cZ){
+        for(let i=1;i<m;i++){
+            arr[i][0] = 0
         }
     }
 };
