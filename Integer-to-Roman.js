84924cf67ad7b8a51f1f153179ed5b53261742c6
Manoj Gadamchetty <manojgadam1@gmail.com>
2025-07-20 17:34:55 -0400

    [12] [Time Beats: 77.85%] [Memory Beats: 92.12%] - LeetPush

--- Integer-to-Roman.js
+++ Integer-to-Roman.js
@@ -3,43 +3,14 @@
  * @return {string}
  */
 var intToRoman = function(num) {
-    const map = {
-        1 : "I",
-        5 : "V",
-        10 : "X",
-        50 : "L",
-        100 : "C",
-        500 : "D",
-        1000 : "M"
-    }
-    let d = 0,res = ""
-    if(num in map)return map[num]
-    while(num > 0){
-        let nn = num%10 * Math.pow(10,d)
-        let curr = ""
-        if(nn in map)curr = map[nn]
-        else{
-            let closest = Math.pow(10,d)
-            if(num%10 == 4){
-                curr = map[closest]+map[5*closest]
-                //console.log(curr)
-            }
-            else if(num%10 == 9){
-                curr = map[closest]+map[closest*10]
-                //console.log(curr)
-            }
-            else if(num%10 == 1 || num%10 == 2 || num%10 == 3){
-               for(let i=0;i<num%10;i++)curr += map[closest]
-            }
-            else if(num%10 == 6 || num%10 == 7 || num%10 == 8){
-                curr = map[5*closest]
-                let c = num%10-5
-                for(let i=0;i<c;i++)curr += map[closest]
-            }
+    let values=  [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    let symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    let res = ""
+    for(let i=0;i<values.length;i++){
+        while(num >= values[i]){
+            res += symbols[i]
+            num -= values[i]
         }
-        res =curr+res
-        d++
-        num = Math.floor(num/10)
     }
     return res
 };
