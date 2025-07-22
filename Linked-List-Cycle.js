7e9a45e25fb385484a86f21049fb8fd57dfbf7b9
Manoj Gadamchetty <manojgadam1@gmail.com>
2025-07-22 19:25:39 -0400

    [141] [Time Beats: 20.79%] [Memory Beats: 67.12%] - LeetPush

--- Linked-List-Cycle.js
+++ Linked-List-Cycle.js
@@ -11,12 +11,11 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    if(!head)return false
-    let slow = head, fast = head, i = 0
-    while(slow.next && fast.next && fast.next.next){
-        slow = slow.next
+    let fast = head, slow = head
+    while(fast && fast.next){
+        slow=  slow.next
         fast = fast.next.next
-        if(slow == fast)return true
+        if(fast == slow)return true
     }
     return false
 };
