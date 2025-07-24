6a2b67ca91785546235c4e5d24a68cb4a98985a6
Manoj Gadamchetty <manojgadam1@gmail.com>
2025-07-24 15:31:34 -0400

    [198] [Time Beats: 100.00%] [Memory Beats: 41.13%] - LeetPush

--- House-Robber.js
+++ House-Robber.js
@@ -6,7 +6,7 @@ var rob = function(nums) {
     const rec=(i,map)=>{
         if( i in map)return map[i]
         if(i>nums.length-1)return 0
-        map[i] = nums[i]+Math.max(rec(i+2,map),rec(i+3,map))
+        map[i] = Math.max(nums[i]+rec(i+2,map),rec(i+1,map))
         return map[i]
     }
     return Math.max(rec(0,{}),rec(1,{}))
